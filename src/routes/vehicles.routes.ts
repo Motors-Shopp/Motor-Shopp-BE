35234bd02a28948b05003507d069d84ce95e54da
Bruno Gomes <gomesbruno030@outlook.com>
2022-12-22 13:35:44 -0300

    fix rota de criar anuncio e mudança na entities

--- src/routes/vehicles.routes.ts
+++ src/routes/vehicles.routes.ts
@@ -1,10 +1,11 @@
-import { getVehicleController, getCarByIDController, createVehicleController } from '@controllers/vehicle.controllers';
+import { createVehicleController, getVehicleController } from '@controllers/vehicle.controllers';
+// import { getVehicleController, getCarByIDController, createVehicleController } from '@controllers/vehicle.controllers';
 import { Router } from 'express';
 
 export const vehiclesRouter = Router();
 
 vehiclesRouter.get("", getVehicleController) 
-vehiclesRouter.get("/:id", getCarByIDController)
+// vehiclesRouter.get("/:id", getCarByIDController)
 vehiclesRouter.post("", createVehicleController);
 /*vehiclesRouter.delete("/:id", );
 vehiclesRouter.patch("/:id", );
