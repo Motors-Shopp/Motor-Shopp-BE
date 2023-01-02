4f61318382a7053a83da31dfe6c394f06d44c628
José Duarte <joseduartent@gmail.com>
2023-01-02 18:39:47 -0400

    chore: continuando criação do anuncio e ajustando o typescript

--- src/interfaces/vehicles.interface.ts
+++ src/interfaces/vehicles.interface.ts
@@ -1,12 +1,16 @@
 
 export interface ICreateVehicle {
-    name: string;
+    title: string;
     model: string;
     localization: string; 
+    description: string;
     year: number;
     plate: string; 
     kilometers: number;
     price: number;  
+    typeOfVehicle: string;
+    img: string;
+    fristImg: string;
 }
 
 export interface IGetCarsById {
