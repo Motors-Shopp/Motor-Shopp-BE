ce60ce1badbe0cb1812c16ec94d5725f02951a50
anjosdelacerda <anjosdelacerda@gmail.com>
2023-01-04 08:19:53 -0300

    CREATE: Criação da rota users

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
 import { vehiclesRouter } from "@routes/vehicles.routes";
-
+import { Router } from "express";
+import { usersRouter } from "./users.routes";
 
 export const AppRoutes = Router();
 
 AppRoutes.use("/vehicles", vehiclesRouter);
-
+AppRoutes.use("/users", usersRouter);
