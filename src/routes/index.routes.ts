429ea62b09ac5f98a709aa4e5b9a46f7b21bc1dc
anjosdelacerda <anjosdelacerda@gmail.com>
2023-01-09 14:16:27 -0300

    n/d

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,9 @@
 import { commentsRouter } from "@routes/comments.routes";
 import { vehiclesRouter } from "@routes/vehicles.routes";
 import { Router } from "express";
+import { recoveryPassRouter } from "./recoveryPassword.routes";
 import { sessionRouter } from "./session.routes";
 import { usersRouter } from "./users.routes";
-import { commentsRouter } from "@routes/comments.routes";
-import { recoveryPassRouter } from "./recoveryPassword.routes";
-
 
 export const AppRoutes = Router();
 
@@ -14,7 +12,6 @@ AppRoutes.use("/users", usersRouter);
 AppRoutes.use("/comments", commentsRouter);
 
 //recovery password route
-AppRoutes.use("/password", recoveryPassRouter)
+AppRoutes.use("/password", recoveryPassRouter);
 
 AppRoutes.use("/login", sessionRouter);
-
